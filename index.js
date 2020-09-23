67ad5ef032d40292c594c3a785e4da36b0deca37
Ryan O'Connor <rocsteady888@gmail.com>
2020-09-23 12:47:46 -0400

    added parameter for app.use()

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const express = require("express");
+const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
 
 // Connect Database
 connectDB();
+
+// Init Middleware
+app.use(express.json({extended: false}));
 
 // Define Routes
 app.use('/api/users', require('./api/users'));
