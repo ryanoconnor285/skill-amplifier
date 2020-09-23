05c779524499d6fe1b2f5f9ad9db39e2de8fe27c
Ryan O'Connor <rocsteady888@gmail.com>
2020-09-23 13:18:14 -0400

    added dotenv

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
@@ -13,6 +14,6 @@ app.use('/api/users', require('./api/users'));
 app.use('/api/auth', require('./api/auth'));
 app.use('/api/profile', require('./api/profile'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
