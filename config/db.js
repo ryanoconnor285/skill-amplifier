e32c9b8cf4a2504e9d4439b22e94dc19470e3648
Ryan O'Connor <rocsteady888@gmail.com>
2020-09-23 13:18:54 -0400

    added dotenv for mongoURI

--- config/db.js
+++ config/db.js
@@ -1,10 +1,9 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+    await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
     console.log('Database connected successfully')
   } catch (err) {
     console.error(err.message);
