103a052d97d7e00a2d094fffb156a4c07a655f37
Ryan O'Connor <rocsteady888@gmail.com>
2020-09-23 12:48:24 -0400

    added test route for user email

--- api/users.js
+++ api/users.js
@@ -1,10 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User');
 
 // @route   GET api/users
 // @desc    Test route
 // @access  Public
 
-router.get('/', (req, res) => res.send('User Route'));
+router.get('/', [
+  check('email', 'Email is required.').not().isEmpty(),
+  check('email', 'Please enter a valid email.').isEmail(),
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+
+  const { email } = req.body;
+
+  try {
+
+    // Find user by email
+    let user = await User.findOne({email})
+
+    if (!user) {
+      console.log('email not found')
+    } else {
+      console.log(user)
+    }
+
+  } catch (error) {
+    console.error(error);
+  }
+
+  res.send('User Route')
+});
 
 module.exports = router;
