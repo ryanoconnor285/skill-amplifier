72df58571f6bd802f2c76c0e0f638d0e84c1d388
Ryan O'Connor <rocsteady888@gmail.com>
2020-09-23 12:48:40 -0400

    added parameter for db connection

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
     console.log('Database connected successfully')
   } catch (err) {
     console.error(err.message);
