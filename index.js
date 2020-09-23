3cdf8056f48c164c21d84ce6409e1ff3147521bc
Ryan O'Connor <rocsteady888@gmail.com>
2020-09-23 08:34:33 -0400

    added routes

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ const app = express();
 // Connect Database
 connectDB();
 
-const PORT = process.env.PORT || 5000;
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
 
-app.use((req, res, next) => {
-  res.send("<h1>Hello World</h>");
-});
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
