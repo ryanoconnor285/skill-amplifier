c89c429be75b12cbd6028d742372ef504a45b9eb
Ryan OConnor <ryanoconnor@Ryans-MacBook-Pro.local>
2020-10-29 10:30:23 -0400

    added register route

--- api/users.js
+++ api/users.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const {
+  check,
+  validationResult
+} = require('express-validator');
 
 const User = require('../models/User');
 
@@ -14,15 +18,21 @@ router.get('/', [
 ], async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() })
+    return res.status(400).json({
+      errors: errors.array()
+    })
   }
 
-  const { email } = req.body;
+  const {
+    email
+  } = req.body;
 
   try {
 
     // Find user by email
-    let user = await User.findOne({email})
+    let user = await User.findOne({
+      email
+    })
 
     if (!user) {
       console.log('email not found')
@@ -36,5 +46,64 @@ router.get('/', [
 
   res.send('User Route')
 });
+
+// @route POST api/users
+// @desc Register user
+// @ access Pulic
+router.post('/',
+  [
+    check('firstName', 'First name is required.').not().isEmpty(),
+    check('lastName', 'Last name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters')
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array()
+      });
+    }
+
+    const {
+      firstName,
+      lastName,
+      email,
+      password
+    } = req.body;
+
+    try {
+      let user = await User.findOne({
+        email
+      });
+
+      if (user) {
+        return res.status(400).json({
+          errors: [{
+            msg: 'User already exists '
+          }]
+        })
+      }
+
+      user = new User({
+        firstName,
+        lastName,
+        email,
+        password
+      })
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.send('User Registered');
+
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error')
+    }
+  })
 
 module.exports = router;
