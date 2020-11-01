1eece9f8bf66f6eb9943176b66e018cc45946c31
Ryan OConnor <ryanoconnor@Ryans-MacBook-Pro.local>
2020-11-01 12:27:59 -0500

    added parameters to mongoose

--- config/db.js
+++ config/db.js
@@ -3,7 +3,11 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true
+    });
     console.log('Database connected successfully')
   } catch (err) {
     console.error(err.message);
