40b051bb5d24e05cda32a28a64d771725b65dbee
Ryan OConnor <ryanoconnor@Ryans-MacBook-Pro.local>
2020-11-01 12:27:44 -0500

    added post route for registered user to get a token

--- api/users.js
+++ api/users.js
@@ -1,67 +1,73 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const {
   check,
   validationResult
-} = require('express-validator');
+} = require("express-validator");
 
-const User = require('../models/User');
+const User = require("../models/User");
 
 // @route   GET api/users
 // @desc    Test route
 // @access  Public
 
-router.get('/', [
-  check('email', 'Email is required.').not().isEmpty(),
-  check('email', 'Please enter a valid email.').isEmail(),
-], async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({
-      errors: errors.array()
-    })
-  }
-
-  const {
-    email
-  } = req.body;
-
-  try {
-
-    // Find user by email
-    let user = await User.findOne({
-      email
-    })
-
-    if (!user) {
-      console.log('email not found')
-    } else {
-      console.log(user)
-    }
-
-  } catch (error) {
-    console.error(error);
-  }
-
-  res.send('User Route')
-});
-
-// @route POST api/users
-// @desc Register user
-// @ access Pulic
-router.post('/',
+router.get(
+  "/",
   [
-    check('firstName', 'First name is required.').not().isEmpty(),
-    check('lastName', 'Last name is required').not().isEmpty(),
-    check('email', 'Please include a valid email').isEmail(),
-    check('password', 'Please enter a password with 6 or more characters')
+    check("email", "Email is required.").not().isEmpty(),
+    check("email", "Please enter a valid email.").isEmail(),
   ],
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({
-        errors: errors.array()
+        errors: errors.array(),
+      });
+    }
+
+    const {
+      email
+    } = req.body;
+
+    try {
+      // Find user by email
+      let user = await User.findOne({
+        email,
+      });
+
+      if (!user) {
+        console.log("email not found");
+      } else {
+        console.log(user);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+
+    res.send("User Route");
+  }
+);
+
+// @route POST api/users
+// @desc Register user
+// @ access Public
+router.post(
+  "/",
+  [
+    check("firstName", "First name is required.").not().isEmpty(),
+    check("lastName", "Last name is required").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Please enter a password with 6 or more characters").isLength({
+      min: 6
+    }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
       });
     }
 
@@ -74,23 +80,23 @@ router.post('/',
 
     try {
       let user = await User.findOne({
-        email
+        email,
       });
 
       if (user) {
         return res.status(400).json({
           errors: [{
-            msg: 'User already exists '
-          }]
-        })
+            msg: "User already exists ",
+          }, ],
+        });
       }
 
       user = new User({
         firstName,
         lastName,
         email,
-        password
-      })
+        password,
+      });
 
       const salt = await bcrypt.genSalt(10);
 
@@ -98,12 +104,29 @@ router.post('/',
 
       await user.save();
 
-      res.send('User Registered');
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
 
+      jwt.sign(
+        payload,
+        process.env.JWT_SECRET, {
+          expiresIn: 3600
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({
+            token
+          });
+        }
+      );
     } catch (err) {
       console.error(err.message);
-      res.status(500).send('Server error')
+      res.status(500).send("Server error");
     }
-  })
+  }
+);
 
 module.exports = router;
