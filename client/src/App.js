84be814a5dbca328c8225cc8f716c3ae17398f6b
Ryan OConnor <ryanoconnor@Ryans-MacBook-Pro.local>
2020-11-06 15:02:44 -0500

    brought in alert from store

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,54 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 import Landing from "./pages/landing/landing.page";
 import Login from "./pages/login/login.page";
 import Navbar from "./components/navbar/navbar.component";
+import Alert from "./components/alert/alert.component";
 
 //Redux
-import { Provider } from "react-redux";
+import {
+  Provider
+} from "react-redux";
 import store from "./store";
 
 function App() {
-  return (
-    <Provider store={store}>
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/login" component={Login} />
-        </Switch>
-      </Router>
-    </Provider>
+  return ( <
+    Provider store = {
+      store
+    } >
+
+    <
+    Router >
+
+    <
+    Navbar / >
+    <
+    Alert / >
+    <
+    Switch >
+    <
+    Route exact path = "/"
+    component = {
+      Landing
+    }
+    /> <
+    Route exact path = "/login"
+    component = {
+      Login
+    }
+    /> <
+    /Switch>
+
+    <
+    /Router>
+
+    <
+    /Provider>
   );
 }
 
