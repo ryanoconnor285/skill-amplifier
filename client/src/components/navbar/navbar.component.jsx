0f36dfddaff462237cf9deab7006d7267d1daf43
Ryan OConnor <ryanoconnor@Ryans-MacBook-Pro.local>
2020-11-06 15:03:29 -0500

    changed to css

--- client/src/components/navbar/navbar.component.jsx
+++ client/src/components/navbar/navbar.component.jsx
@@ -1,11 +1,11 @@
-import './navbar.styles.scss';
+import "./navbar.styles.css";
 
 const Navbar = () => {
   return (
     <div>
       <h1>Navbar</h1>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar;
--- client/src/pages/login/login.page.jsx
+++ client/src/pages/login/login.page.jsx
@@ -1,15 +1,19 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { setAlert } from "../../actions/alert";
+import PropTypes from "prop-types";
+import axios from "axios";
 
-function Login() {
+function Login({ setAlert }) {
   const [formData, setFormData] = useState({
     email: "roconnor@orangecountync.gov",
-    password: "high-Falcon"
+    password: "high-Falcon",
   });
 
   const { email, password } = formData;
 
-  const handleChange = e => setFormData({ ...formData, [e.target.name]: e.target.value })
+  const handleChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -17,47 +21,53 @@ function Login() {
     try {
       const config = {
         headers: {
-          'Content-Type': 'application/json'
-        }
-      }
+          "Content-Type": "application/json",
+        },
+      };
 
       const body = JSON.stringify(formData);
 
-      const res = await axios.post('api/auth', body, config);
+      const res = await axios.post("api/auth", body, config);
       console.log(res);
     } catch (err) {
-      console.error(err.response.data)
+      console.error(err.response.data);
+      setAlert(
+        "An error occurred while submitting the login form. Server responded with a status code of " +
+          err.response.status
+      );
     }
-  }
+  };
 
   return (
     <div>
       <h1>Login</h1>
-      <form onSubmit={e => handleSubmit(e)}>
-
-        <input 
-          type="email" 
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input
+          type="email"
           placeholder="Email"
-          name="email" 
-          value={email} 
-          onChange={e => handleChange(e)} 
-          required 
+          name="email"
+          value={email}
+          onChange={(e) => handleChange(e)}
+          required
         />
 
-        <input 
-          type="password" 
-          placeholder="Passoword" 
-          name="password" 
-          value={password} 
-          onChange={e => handleChange(e)} 
-          required 
+        <input
+          type="password"
+          placeholder="Passoword"
+          name="password"
+          value={password}
+          onChange={(e) => handleChange(e)}
+          required
         />
 
-        <button onClick={e => handleSubmit(e)}>Submit</button>
-
+        <button onClick={(e) => handleSubmit(e)}>Submit</button>
       </form>
     </div>
-  )
+  );
 }
 
-export default Login;
+Login.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+};
+
+export default connect(null, { setAlert })(Login);
