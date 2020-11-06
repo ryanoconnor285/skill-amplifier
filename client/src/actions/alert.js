83b9aaca8e793efcaee7502b65c5557e5c0f646e
Ryan OConnor <ryanoconnor@Ryans-MacBook-Pro.local>
2020-11-06 15:03:03 -0500

    added universal id and dispatch

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,10 +1,31 @@
-import uuid from 'uuid';
-import { SET_ALERT, REMOVE_ALERT } from './types';
+import {
+  v4 as uuidv4
+} from 'uuid';
+import {
+  SET_ALERT,
+  REMOVE_ALERT
+} from './types';
 
 export const setAlert = (msg, alertType) => dispatch => {
-  const id = uuid.v4();
+  const id = uuidv4();
   dispatch({
     type: SET_ALERT,
-    payload: { msg, alertType, id }
+    payload: {
+      msg,
+      alertType,
+      id
+    }
+  });
+}
+
+export const removeAlert = (msg, alertType) => dispatch => {
+  const id = uuidv4();
+  dispatch({
+    type: REMOVE_ALERT,
+    payload: {
+      msg,
+      alertType,
+      id
+    }
   });
 }
