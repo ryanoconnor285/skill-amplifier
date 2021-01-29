9ddc932b3b15295fbeaae2ea2434d98798ba2ecd
Ryan OConnor <ryanoconnor@Ryans-MacBook-Pro.local>
2021-01-28 22:21:10 -0500

    removed unecessary routes

--- index.js
+++ index.js
@@ -12,10 +12,6 @@ app.use(express.json({ extended: false }));
 // Define Routes
 app.use("/api/users", require("./api/users"));
 app.use("/api/auth", require("./api/auth"));
-app.use("/api/profile", require("./api/profile"));
-app.use("/reports", require("./api/reports"));
-app.use("/incidents", require("./api/incidents"));
-app.use("/clinicians", require("./api/clinicians"));
 
 const PORT = process.env.PORT || 5001;
 
