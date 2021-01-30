10392ca90eaaa865362560e5f5a631f97208ae74
Ryan OConnor <ryanoconnor@Ryans-MacBook-Pro.local>
2021-01-30 09:09:47 -0500

    implemented multer

--- api/posts.js
+++ api/posts.js
@@ -1,40 +1,57 @@
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
+const path = require("path");
+const multer = require("multer");
 const auth = require("../middleware/auth");
 const { check, validationResult } = require("express-validator");
 
+//  Models
+const User = require("../models/User");
 const Post = require("../models/Post");
+
+//  File Storage
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage: storage });
 
 // @route POST api/posts
 // @desc Upload a post
 // @ access Private
-router.post(
-  "/",
-  [
-    check("descShort", "descShort is required.").not().isEmpty(),
-    check("descLong", "descLong is required").not().isEmpty(),
-  ],
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({
-        errors: errors.array(),
-      });
-    }
+router.post("/", auth, upload.single("tracingImage"), async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      errors: errors.array(),
+    });
+  }
 
-    const { descShort, descLong } = req.body;
+  try {
+    const user = (await User.findById(req.user.id)).isSelected("-password");
 
-    const post = new Post();
-    post.descShort = descShort;
-    post.descLong = descLong;
-    // post.img.data = fs.readFileSync(req.files.userPhoto.path);
-    // post.img.contentType = `image/png`;
-    await post.save();
-    console.log("Post Route", req.body);
+    const newPost = new Post({
+      title: req.body.title,
+      desc: req.body.desc,
+      user: req.user.id,
+      img: {
+        data: fs.readFileSync(req.file.path),
+        contentType: "image/jpeg",
+      },
+    });
+
+    const post = await newPost.save();
 
     res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
   }
-);
+});
 
 module.exports = router;
