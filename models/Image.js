960611f4eddcc061882f6cc9064e2440d41e397a
Ryan OConnor <ryanoconnor@Ryans-MacBook-Pro.local>
2021-02-22 19:39:24 -0500

    syntax change

--- models/Image.js
+++ models/Image.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const ImageSchema = new mongoose.Schema({
+const ImageSchema = new Schema({
   user: {
     type: Schema.Types.ObjectId,
     ref: "users",
