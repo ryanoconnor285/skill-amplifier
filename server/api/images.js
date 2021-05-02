3c512af3d47dc1ba6703e4423e101df74a4ce43f
Ryan OConnor <ryanoconnor@Ryans-MacBook-Pro.local>
2021-05-01 22:04:48 -0400

    added route for getting an image for authenticated user

--- server/api/images.js
+++ server/api/images.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/auth");
-const uploadController = require("./../controllers/upload");
+const {
+  uploadImages,
+  resizeImages,
+  getResult,
+} = require("../middleware/upload");
+const { getFileStream } = require("../middleware/s3");
+
+// @route POST api/images/:imageId
+// @desc Get image from bucket
+// @access Private
+router.get("/:imageId", auth, getFileStream);
 
 // @route POST api/images
 // @desc Upload images to a folder on the server
 // @access Private
-router.post(
-  "/",
-  auth,
-  uploadController.uploadImages,
-  uploadController.resizeImages,
-  uploadController.getResult
-);
+router.post("/", auth, uploadImages, resizeImages, getResult);
 
 module.exports = router;
