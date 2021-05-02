074999d884f64146f78d5f2e02c0662c85012ae2
Ryan OConnor <ryanoconnor@Ryans-MacBook-Pro.local>
2021-05-01 22:03:56 -0400

    added images to root reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,5 +2,6 @@ import { combineReducers } from "redux";
 import alert from "./alert";
 import auth from "./auth";
 import post from "./post";
+import images from "./images";
 
-export default combineReducers({ alert, auth, post });
+export default combineReducers({ alert, auth, post, images });
