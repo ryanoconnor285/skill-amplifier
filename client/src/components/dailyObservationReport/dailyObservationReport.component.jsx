db3a761f86d0c994be9729344d44b805c190063d
Ryan O'Connor <rocsteady888@gmail.com>
2021-05-11 18:01:29 -0400

    initial commit

---
+++ client/src/components/dailyObservationReport/dailyObservationReport.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const DailyObservationReport = () => {
+  return (
+    <div>
+      <h2>Daily Observation Report</h2>
+      <h4>Field Training Performance Evaluation Factors</h4>
+      <form>
+        <fieldset>
+          <legend>Fruit juice size</legend>
+          <p>
+            <input type="radio" name="size" id="size_1" value="small" />
+            <label for="size_1">Small</label>
+          </p>
+          <p>
+            <input type="radio" name="size" id="size_2" value="medium" />
+            <label for="size_2">Medium</label>
+          </p>
+          <p>
+            <input type="radio" name="size" id="size_3" value="large" />
+            <label for="size_3">Large</label>
+          </p>
+        </fieldset>
+      </form>
+    </div>
+  );
+};
+
+export default DailyObservationReport;
