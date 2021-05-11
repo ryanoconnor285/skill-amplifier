939565b4f70a4fcde598e944b442852ca09d1dd1
Ryan O'Connor <rocsteady888@gmail.com>
2021-05-11 18:01:41 -0400

    initial commit

---
+++ client/src/pages/evaluations/evaluations.pages.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PostCard from "../../components/postCard/postCard.component";
+
+function Evaluations() {
+  return (
+    <div>
+      <PostCard />
+    </div>
+  );
+}
+
+export default Evaluations;
