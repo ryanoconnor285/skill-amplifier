b1e27aa9ebf64556a09ca65133608c0cf5977778
Ryan O'Connor <rocsteady888@gmail.com>
2021-06-01 13:37:01 -0400

    added radio button

--- client/src/components/formComponents/radioInput/radioInput.component.jsx
+++ client/src/components/formComponents/radioInput/radioInput.component.jsx
@@ -1,15 +1,21 @@
 import React from "react";
-import "./radioInput.style.sass";
+import "./radioInput.style.css";
 
 const RadioInputs = ({ legend, description, values, className }) => {
   return (
     <fieldset className={className}>
-      <legend>{legend}</legend>
-      <p>{description}</p>
+      <div>
+        <legend>{legend}</legend>
+      </div>
+      <div>
+        <p>{description}</p>
+      </div>
       {values.map((value) => (
         <div key={legend.trim().toLowerCase() + "-" + value}>
-          <input type="radio" name={legend} id={value} value={value} />
-          <label htmlFor={value}>{value}</label>
+          <label htmlFor={value}>
+            <input type="radio" name={legend} id={value} value={value} />
+            {value}
+          </label>
         </div>
       ))}
     </fieldset>
